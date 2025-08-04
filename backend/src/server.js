8e852db7b002d85821d1607820fa5c494cadcbbf
Himanshi Negi <himanshinegi401@gmail.com>
2025-08-04 20:04:07 +0530

    Project Completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ import path from "path";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 // Middleware
